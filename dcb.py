f31b90866b0f06b02ba73cab661c086e63fcb544
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-02-18 11:38:02 +0000

    Added basics for pumps and Rheodyne valves

---
+++ dcb.py
@@ -0,0 +1,50 @@
+import pumpy.pumpy as pumpy
+import rheodyne.rheodyne as rheodyne
+import math
+
+# Variables
+IL_length = 100 # mm
+IL_ID = 1 # mm
+IL_vol = math.pi * (IL_ID/2.0)**2 * IL_length # uL
+waste = 10 # waste position on selector
+waste_rinse_vol = 100 # uL
+fr_carrier = 100 # uL/min
+fr_substrate = 100 # uL/min
+
+# Configure Arduino Rheodyne selector/six-port valve controller
+rheo = rheodyne.Rheodyne('COM7')
+
+# Configure pump chain
+chain = pumpy.Chain('COM1')
+
+# Configure additive withdrawer - withdraws from selector and into IL
+pull = pumpy.Pump(chain,1) # 
+pull.setdiameter(14.5)
+pull.setflowrate(500)
+
+# Configure additive pusher - pushes additive out of IL into reactor
+push = pumpy.Pump(chain,2) 
+push.setdiameter(14.5)
+push.setflowrate(500)
+
+# Configure carrier fluid
+carrier = pumpy.Pump(chain,3)
+carrier.setdiameter(15)
+carrier.setflowrate(fr_carrier)
+
+# Configure substrate and catalyst (same pump, PHD2000)
+subcat = pumpy.PHD2000(chain,4)
+subcat.setdiameter(15)
+subcat.setflowrate(fr_substrate)
+
+# Tests
+for pump in [pull, push, carrier, subcat]:
+    pump.infuse()
+
+rheo.valve(True)
+rheo.selector(2)
+rheo.valve(False)
+rheo.selector(7)
+
+for pump in [pull, push, carrier, subcat]:
+    pump.stop()
