1654105f08ac43345d88cd3ace62324dd3539781
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-03-13 16:23:38 +0000

    Added argparse, error checking for selector

--- rheodyne/rheodyne.py
+++ rheodyne/rheodyne.py
@@ -1,6 +1,7 @@
 from __future__ import print_function 
 import serial
 import time
+import argparse
 
 # Selector
 class Rheodyne:
@@ -12,14 +13,16 @@ class Rheodyne:
         else:
             error('Rheodyne: ERROR - not opened on',port)
 
-    
     def selector(self,position):
-        self.serial.write('S' + str(position-1))
-        response = self.serial.readline()
-        if not response.startswith('done'):
-            self.error('Rheodyne: unexpected response from selector',response)
+        if position > 0 and position < 11:
+            self.serial.write('S' + str(position-1))
+            response = self.serial.readline()
+            if not response.startswith('done'):
+                self.error('Rheodyne: unexpected response from selector',response)
+            else:
+                print('Rheodyne: selector set to',position)
         else:
-            print('Rheodyne: selector set to',position)
+            print('ERROR: invalid position')
 
     def valve(self,inject):
         if inject:
@@ -46,3 +49,29 @@ class Rheodyne:
 
     def error(*msg):
         print('Error:',msg)
+
+# Command line options
+# Run with -h flag to see help
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Command line interface to control Rheodyne six-port and selector valve via an Arduino')
+    parser.add_argument('port',help='serial port')
+    # selector
+    parser.add_argument('-s',dest='selector',help='selector position',type=int)
+    group = parser.add_mutually_exclusive_group()
+    # six port
+    group.add_argument('-inject',action='store_true')
+    group.add_argument('-load',action="store_true")
+    args = parser.parse_args()
+
+    rheo = Rheodyne(args.port)
+
+    if args.selector:
+        rheo.selector(args.selector)
+
+    if args.inject:
+        rheo.valve(True)
+    elif args.load:
+        rheo.valve(False)
+
+    rheo.close()
