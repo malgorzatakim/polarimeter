df0002d3ab7b50640bd3d55d39b9e1ee4654f32b
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-12-03 11:26:22 +0000

    Import bitlib as bl not *

--- polarimeter/acquire_data.py
+++ polarimeter/acquire_data.py
@@ -1,5 +1,5 @@
 from __future__ import division
-from bitlib import *
+import bitlib as bl
 import argparse
 import os
 
@@ -15,42 +15,42 @@ def main(samples,sample_rate):
     the directory at the end. Terrible."""
     intial_dir = os.getcwd()
 
-    assert BL_Open() == 1 # returns number devices opened
+    assert bl.BL_Open() == 1 # returns number devices opened
 
     """First, must select device. Returns index of selected entity.
     Note that it is 0-indexed."""
-    assert BL_Select(BL_SELECT_DEVICE,0) == 0
+    assert bl.BL_Select(bl.BL_SELECT_DEVICE,0) == 0
 
     """Second, select the capture mode. This must happen after
     selecting the device but before selecting channels."""
-    BL_Mode(BL_MODE_DUAL) # prefered capture mode
+    bl.BL_Mode(bl.BL_MODE_DUAL) # prefered capture mode
 
     """Now select each channel in turn and configure. Returns index
     of selected channel if channel has been successfully
     selected."""
     channels = [0,1]
     for channel in channels:
-        assert BL_Select(BL_SELECT_CHANNEL,channel) == channel
+        assert bl.BL_Select(bl.BL_SELECT_CHANNEL,channel) == channel
 
         # not sure if this should be bnc
-        BL_Select(BL_SELECT_SOURCE,BL_SOURCE_POD)
+        bl.BL_Select(bl.BL_SELECT_SOURCE,bl.BL_SOURCE_POD)
 
         """Set the voltage range. Can get the number of ranges from
-        BL_Count(BL_COUNT_RANGE). This returns 5. Can set the maxi
-        range using BL_Range(BL_Count(BL_COUNT_RANGE) (11 V) but I
-        only want 5 V. This is an index of 3. BL_Range returns max
-        voltage. """
-        assert BL_Range(3) == 5.2
+        bl.BL_Count(bl.BL_COUNT_RANGE). This returns 5. Can set the max
+        range using bl.BL_Range(bl.BL_Count(bl.BL_COUNT_RANGE) (11 V)
+        but I only want 5 V. This is an index of 3. bl.BL_Range returns
+        max voltage. """
+        assert bl.BL_Range(3) == 5.2
         
         # Enable the channel
-        assert BL_Enable(True) == True
+        assert bl.BL_Enable(True) == True
 
-    """Now do trace parameters. BL_Rate must always be specified
+    """Now do trace parameters. bl.BL_Rate must always be specified
     and it should be the first parameter assigned when preparing a
     new trace. Note that if this sample rate can't be selected, the
     nearest sample rate will be returned."""
     try:
-        actual_sample_rate = BL_Rate(sample_rate)
+        actual_sample_rate = bl.BL_Rate(sample_rate)
         assert sample_rate == actual_sample_rate
     except AssertionError:
         raise AssertionError('Maximum sample rate of %i sps available. '
@@ -60,26 +60,26 @@ def main(samples,sample_rate):
     """Next is number of samples. Returned value is actual number of
     samples returned."""
     try:
-        actual_samples = BL_Size(samples)
+        actual_samples = bl.BL_Size(samples)
         assert samples == actual_samples
     except AssertionError:
         raise AssertionError('Maximum of %i samples available. Requested %i '
                              'samples.' % (actual_samples, samples))
 
     # do trace, acquire data
-    BL_Trace()
+    bl.BL_Trace()
 
     # get first channel
     channel = 0
-    assert BL_Select(BL_SELECT_CHANNEL,channel) == channel
-    chA = BL_Acquire()
+    assert bl.BL_Select(bl.BL_SELECT_CHANNEL,channel) == channel
+    chA = bl.BL_Acquire()
 
     # get second channel
     channel = 1
-    assert BL_Select(BL_SELECT_CHANNEL,channel) == channel
-    chB = BL_Acquire()
+    assert bl.BL_Select(bl.BL_SELECT_CHANNEL,channel) == channel
+    chB = bl.BL_Acquire()
 
-    BL_Close()
+    bl.BL_Close()
 
     # provide times samples were taken
     time = [t / sample_rate for t in range(0, samples)]
