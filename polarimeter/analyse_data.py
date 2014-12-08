9316181ffaca7a1853e92678abfbbe8134f6a4fa
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-12-08 14:21:14 +0000

    Created polarimeter data class

--- polarimeter/analyse_data.py
+++ polarimeter/analyse_data.py
@@ -2,37 +2,47 @@ from __future__ import division
 import numpy as np
 from scipy.fftpack import ifft, fft, fftfreq
 
-def calc_phase_difference(t, ref, obj):
-	"""Calculate the phase difference between the reference and object
-	signals. Returns phase difference in radians.
-	"""
-	f = fftfreq(len(ref), t[1]-t[0])
+class PolarimeterData:
+	def __init__(self, t, ref, obj):
+		self.t = t
+		self.ref = ref
+		self.obj = obj
+		self.calc_phase_difference()
 
-	# low pass filter
-	lp_filter = np.ones(len(f)) / (1 + ((f / 6)**2))   
-	ref = ifft(fft(ref) * lp_filter)
-	obj = ifft(fft(obj) * lp_filter)
+	def calc_phase_difference(self):
+		"""Calculate the phase difference between the reference and object
+		signals. Returns phase difference in radians.
+		"""
+		t = self.t
+		ref = self.ref
+		obj = self.obj
 
-	# apodising filter (time domain)
-	a0 = 0.355768
-	a1 = 0.487396
-	a2 = 0.144232
-	a3 = 0.012604
-	n = np.arange(0,len(t),1)
-	apodize_filter =  (a0 - a1*np.cos(2*np.pi*n/len(t))
-					  + a2*np.cos(4*np.pi*n/len(t))
-					  - a3*np.cos(6*np.pi*n/len(t)))
-	ref *= apodize_filter
-	obj *= apodize_filter
+		f = fftfreq(len(ref), t[1]-t[0])
 
-	# bp filter in freq domain
-	sig = 1
-	f0 = 7
-	bp_filter = np.exp(-(f-f0)**2 / (2*sig*sig))    
-	ref = ifft(fft(ref) * bp_filter)
-	obj = ifft(fft(obj) * bp_filter)
+		# low pass filter
+		lp_filter = np.ones(len(f)) / (1 + ((f / 6)**2))   
+		ref = ifft(fft(ref) * lp_filter)
+		obj = ifft(fft(obj) * lp_filter)
 
-	delta_phi = np.angle(obj * ref.conjugate()) / 2
+		# apodising filter (time domain)
+		a0 = 0.355768
+		a1 = 0.487396
+		a2 = 0.144232
+		a3 = 0.012604
+		n = np.arange(0,len(t),1)
+		apodize_filter =  (a0 - a1*np.cos(2*np.pi*n/len(t))
+						  + a2*np.cos(4*np.pi*n/len(t))
+						  - a3*np.cos(6*np.pi*n/len(t)))
+		ref *= apodize_filter
+		obj *= apodize_filter
 
-	# exclude outer 25% because of edge effects
-	return np.mean(delta_phi[len(f)*0.25:len(f)*0.75])
+		# bp filter in freq domain
+		sig = 1
+		f0 = 7
+		bp_filter = np.exp(-(f-f0)**2 / (2*sig*sig))    
+		ref = ifft(fft(ref) * bp_filter)
+		obj = ifft(fft(obj) * bp_filter)
+
+		self.delta_phi = np.angle(obj * ref.conjugate()) / 2
+		# exclude outer 25% because of edge effects
+		self.delta_phi = self.delta_phi[len(f)*0.25:len(f)*0.75]
