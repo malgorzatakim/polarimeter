eb2b5c2f4a15726ec05bbda5501bda965749ce1e
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-12-09 12:37:00 +0000

    Now handles multiple measurements

--- polarimeter/analyse_data.py
+++ polarimeter/analyse_data.py
@@ -3,21 +3,48 @@ import numpy as np
 from scipy.fftpack import ifft, fft, fftfreq
 
 class PolarimeterData:
-	def __init__(self, t, ref, obj):
-		self.t = t
+	def __init__(self, time, ref, obj):
+		"""Object to handle polarimeter data and compute phase
+		difference between reference and object beams.
+
+		Arguments:
+			time: list of times corresponding to each sample (seconds)
+			ref: list of signal measurements (volts) for reference beam
+			obj: as for ref but object beam
+
+		Note that ref and obj can be a list of lists, i.e. if repeat
+		measurements were made. 
+
+		Attributes:
+			self.phase_difference: numpy.array of phase difference
+			(radians) between object and reference beam.
+		"""
+
+		# zip below requires list of lists, so check
+		if not any(isinstance(el, list) for el in ref):
+			ref = [ref]
+
+		if not any(isinstance(el, list) for el in obj):
+			obj = [obj]
+
+		self.time = time
 		self.ref = ref
 		self.obj = obj
-		self.calc_phase_difference()
+		self.phase_difference = list()
 
-	def calc_phase_difference(self):
+		for ref, obj in zip(ref, obj):
+			self.phase_difference.append(self._calc_phase_difference(time, obj,
+																	 ref))
+
+		# if only one PD caluclated, return as number not list of 1
+		if len(self.phase_difference) == 1:
+			self.phase_difference = self.phase_difference[0]
+
+	def _calc_phase_difference(self, time, obj, ref):
 		"""Calculate the phase difference between the reference and object
 		signals. Returns phase difference in radians.
 		"""
-		t = self.t
-		ref = self.ref
-		obj = self.obj
-
-		f = fftfreq(len(ref), t[1]-t[0])
+		f = fftfreq(len(ref), time[1]-time[0])
 
 		# low pass filter
 		lp_filter = np.ones(len(f)) / (1 + ((f / 6)**2))   
@@ -29,10 +56,10 @@ class PolarimeterData:
 		a1 = 0.487396
 		a2 = 0.144232
 		a3 = 0.012604
-		n = np.arange(0,len(t),1)
-		apodize_filter =  (a0 - a1*np.cos(2*np.pi*n/len(t))
-						  + a2*np.cos(4*np.pi*n/len(t))
-						  - a3*np.cos(6*np.pi*n/len(t)))
+		n = np.arange(0,len(time),1)
+		apodize_filter =  (a0 - a1*np.cos(2*np.pi*n/len(time))
+						  + a2*np.cos(4*np.pi*n/len(time))
+						  - a3*np.cos(6*np.pi*n/len(time)))
 		ref *= apodize_filter
 		obj *= apodize_filter
 
@@ -43,6 +70,6 @@ class PolarimeterData:
 		ref = ifft(fft(ref) * bp_filter)
 		obj = ifft(fft(obj) * bp_filter)
 
-		self.delta_phi = np.angle(obj * ref.conjugate()) / 2
+		delta_phi = np.angle(obj * ref.conjugate()) / 2
 		# exclude outer 25% because of edge effects
-		self.delta_phi = self.delta_phi[len(f)*0.25:len(f)*0.75]
+		return np.mean(delta_phi[len(f)*0.25:len(f)*0.75])
--- polarimeter/test_analyse_data.py
+++ polarimeter/test_analyse_data.py
@@ -3,8 +3,60 @@ import unittest
 import analyse_data
 import numpy as np
 
-def simulate_signals(t, f=3, phiA=0, phiB=0, dcA=-0.1, dcB=0.2, ampA=1,
-					 ampB=0.7, noise=0):
+class AnalyseDataTestCase(unittest.TestCase):
+	"""Tests for analyse_data.py"""
+	def test_simulate_signals(self):
+		t, a, b = simulate_signals(np.arange(100))
+		self.assertIs(type(t), list)
+		self.assertIs(type(a), list)
+		self.assertIs(type(b), list)
+
+	def test_analysis_no_noise(self):
+		"""Test analysis with a perfect, noiseless signal."""
+		phiB = np.pi * 0.33
+		t, sigA, sigB = simulate_signals(np.arange(0, 1, 0.01), phiB=phiB)
+		p = analyse_data.PolarimeterData(t, sigA, sigB)
+		self.assertEqual(round(phiB, 3), round(p.phase_difference, 3))
+
+	def test_analysis_with_noise(self):
+		"""Test analysis with a noisy simulated signal. Note that this
+		is different to no noise test in that it uses more data (longer
+		time period) and rounds to just 1 d.p. rather than 3.
+		"""
+		phiB = np.pi * 0.33
+		t, sigA, sigB = simulate_signals(np.arange(0, 10, 0.01), phiB=phiB,
+										 noise=0.02)
+		p = analyse_data.PolarimeterData(t, sigA, sigB)
+		self.assertEqual(round(phiB, 1), round(p.phase_difference, 1))
+
+	def test_multiple_signals(self):
+		"""Test analysis with multiple perfect signals to check
+		analyse_data handles input correctly.
+		"""
+		chAs = list()
+		chBs = list()
+		phiB = np.pi * 0.33
+		repeats = 3
+
+		# Generate signals and get into format acquire_data does
+		for i in range(repeats):
+			t, chA, chB = simulate_signals(np.arange(0, 1, 0.01), phiB=phiB)
+			chAs.append(chA) # list of lists of values
+			chBs.append(chB)
+
+		p = analyse_data.PolarimeterData(t, chAs, chBs)
+
+		for i in range(repeats):
+			self.assertEqual(round(phiB, 3), round(p.phase_difference[i], 3))
+
+		self.assertIs(type(p.phase_difference), list)
+
+def simulate_signals(t, f=3, phiA=0, phiB=0, dcA=-0.1, dcB=0.2,
+					  ampA=1, ampB=0.7, noise=0):
+	"""Simulate signals. Returns lists as this is what the
+	acquire_data module does.
+	"""
+	t = np.array(t)
 	random_noise = np.random.normal(size=len(t))
 	sigA = (ampA * (np.cos(2 * np.pi * f * t + phiA)) ** 2 + dcA
 			+ random_noise * noise)
@@ -12,29 +64,7 @@ def simulate_signals(t, f=3, phiA=0, phiB=0, dcA=-0.1, dcB=0.2, ampA=1,
 	random_noise = np.random.normal(size=len(t))
 	sigB = (ampB * (np.cos(2 * np.pi * f * t + phiB)) ** 2 + dcB
 			+ random_noise * noise)
-	return sigA, sigB
-
-class AnalyseDataTestCase(unittest.TestCase):
-	"""Tests for analyse_data.py"""
-
-	def test_analysis_no_noise(self):
-		"""Test analysis with a perfect, noiseless signal."""
-		t = np.arange(0, 1, 0.01)
-		phiB = np.pi * 0.33
-		sigA, sigB = simulate_signals(t, phiB=phiB, noise=0)
-		calc_phiB = analyse_data.calc_phase_difference(t, sigA, sigB)
-		self.assertEqual(round(phiB,3), round(calc_phiB,3))
-
-	def test_analysis_with_noise(self):
-		"""Test analysis with a noisy simulated signal. Note that this
-		is different to no noise test in that it uses more data (longer
-		time period) and rounds to just 2 d.p. rather than 3.
-		"""
-		t = np.arange(0, 10, 0.01)
-		phiB = np.pi * 0.33
-		sigA, sigB = simulate_signals(t, phiB=phiB, noise=0.02)
-		calc_phiB = analyse_data.calc_phase_difference(t, sigA, sigB)
-		self.assertEqual(round(phiB,2), round(calc_phiB,2))
+	return list(t), list(sigA), list(sigB)
 
 if __name__ == '__main__':
 	unittest.main()
