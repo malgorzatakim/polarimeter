8d12e7199343d1b8a474d44985531b6100f48740
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-12-15 10:23:48 +0000

    Merged analyse_data into acquire_data

--- polarimeter/acquire_data.py
+++ polarimeter/acquire_data.py
@@ -1,10 +1,11 @@
 from __future__ import division
 import bitlib as bl
-import argparse
 import logging
 import os
+import numpy as np
+from scipy.fftpack import ifft, fft, fftfreq
 
-def main(rate=5000, size=5000, repeat=1):
+def acquire(rate=5000, size=5000, repeat=1):
     """Acquire data from channels A and B for with specified sample
     rate, capture size and number of repeat measurements.
 
@@ -87,17 +88,54 @@ def main(rate=5000, size=5000, repeat=1):
     time = [t / actual_rate for t in range(0, actual_size)]
     return time, chA, chB
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Acquire data with BitScope on'
-                                     ' channels A and B and print to stdout.')
-    parser.add_argument('time', help='capture time (seconds)', type=float)
-    parser.add_argument('-header', help='print header row', action='store_true')
-    args = parser.parse_args()
+def calc_phase_difference(time, obj, ref):
+    """Calculate the phase difference between the reference and object
+    signals. Returns phase difference in radians.
+    
+    Arguments:
+        time: sampling times
+        obj: object beam signal
+        ref: reference beam signal
 
-    if args.header:
-        print 'time (s), chA (V), chB (V)'
+    Obj and ref can be list of lists of voltages at each sample or a
+    numpy array of size (n, m) where n is the number of repeats and m is
+    the number of samples.
 
-    time, chA, chB = main(args.time)
+    Returns:
+        numpy array of size (n, 1) with each phase difference
+    """
 
-    for t, chA, chB in zip(time, chA[0], chB[0]):
-        print '%f,%f,%f' % (t, chA, chB)
+    time = np.array(time)
+    obj = np.array(obj)
+    ref = np.array(ref)
+
+    f = fftfreq(ref.shape[1], time[1]-time[0])
+
+    # low pass filter
+    lp_filter = np.ones(obj.shape) / (1 + ((f / 6)**2))
+    ref = ifft(fft(ref) * lp_filter)
+    obj = ifft(fft(obj) * lp_filter)
+
+    # apodising filter (time domain)
+    a0 = 0.355768
+    a1 = 0.487396
+    a2 = 0.144232
+    a3 = 0.012604
+    n = np.tile(np.arange(0,len(time),1), (ref.shape[0], 1))
+    apodize_filter =  (a0 - a1*np.cos(2*np.pi*n/len(time))
+                      + a2*np.cos(4*np.pi*n/len(time))
+                      - a3*np.cos(6*np.pi*n/len(time)))
+    ref *= apodize_filter
+    obj *= apodize_filter
+
+    # bp filter in freq domain
+    sig = 1
+    f0 = 7
+    bp_filter = np.tile(np.exp(-(f-f0)**2 / (2*sig*sig)), (ref.shape[0], 1))
+    ref = ifft(fft(ref) * bp_filter)
+    obj = ifft(fft(obj) * bp_filter)
+
+    delta_phi = np.angle(obj * ref.conjugate()) / 2
+    # exclude outer 25% because of edge effects
+    return np.mean(delta_phi[:,delta_phi.shape[1]*0.25:delta_phi.shape[1]*0.75],
+                   axis=1)
