69e27bacd9f00a09b747a01990a6604928d9b8c1
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2015-04-10 13:34:46 +0100

    Added docs

--- polarimeter/labview.py
+++ polarimeter/labview.py
@@ -1,24 +1,29 @@
+"""
+labview.py is a module to interface with a Windows PC running a LabVIEW
+VI that acquires data with an NI-DAQ.
+
+Using TCP/IP the VI listens for incoming connections. The requesting
+computer sends a message requesting a capture time. The Windows PC
+acquires the data, writes it to file, then uses the WinSCP utility to
+copy the file to /home/dcb/polarimeter/data/YYYY-MM-DD-HHMMSS.csv.
+The requesting computer then loads the file, which contais the time
+and signals of the two channels.
+"""
 import socket
 import time
 import numpy as np
 
 socket.setdefaulttimeout(60) # seconds
 
-def acquire(acq_time, IP='155.198.231.92', port=5020):
-	"""Sends a TCP/IP request to the computer at the specified
-	IP address and port to initiate data capture (with acq_time
-	in seconds). Computer will then use WinSCP to copy the file
-	back to this computer in:
-		/home/dcb/polarimeter/data/
-	Function uses returned filename of CSV file to get and return
-	numpy arrays:
-		t, ch A (V), and ch B (V)
+def acquire(capture_time, IP='155.198.231.92', port=5020):
+	"""Aquires data for capture_time (seconds) via acquisition PC.
+	Returns time, and signals of hannel A and channel B.
 	"""
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((IP, port))
-	sock.send('%.2f\r' % acq_time)
+	sock.send('%.2f\r' % capture_time)
 	assert sock.recv(1024) == 'acquiring'
-	time.sleep(acq_time + 1)
+	time.sleep(capture_time + 1)
 	filename = sock.recv(1024)
 	assert filename[-3:] == 'csv'
 	sock.close()
