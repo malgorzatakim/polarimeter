polarimeter.py refactored, fixed BP filter freq Frequency was off by factor of 2

polarimeter/polarimeter.py
@@ -1,25 +1,10 @@
 from __future__ import division
 import numpy as np
 from scipy.fftpack import ifft, fft, fftfreq
-import bitscope
-
-def measure(filename=None, repeat=30):
-    """
-    Acquire signal with default of 30 repeats. Calculate phase difference
-    and write to filename, if provided. Return list of phase differences.
-    All in radians.
-    """
-    t, a, b = bitscope.acquire(repeat=repeat)
-    delta_phi = calc_phase_difference(t, a, b)
-
-    if filename is not None:
-        np.savetxt(filename, delta_phi, delimiter=',')
-
-    return delta_phi
 
 def calc_phase_difference(time, obj, ref):
     """Calculate the phase difference between the reference and object
-    signals. Returns phase difference in radians.
+    signals. Returns phase differences in degrees.
     
     Arguments:
         time: sampling times
@@ -34,36 +19,50 @@ def calc_phase_difference(time, obj, ref):
         numpy array of size (n, 1) with each phase difference
     """
 
-    # time = np.array(time)
-    # obj = np.array(obj)
-    # ref = np.array(ref)
+    obj = low_pass_filter(time, obj)
+    ref = low_pass_filter(time, ref)
 
-    f = fftfreq(len(ref), time[1]-time[0])
+    obj = apodise(time, obj)
+    ref = apodise(time, ref)
 
-    # low pass filter
-    lp_filter = np.ones(len(obj)) / (1 + ((f / 6)**2))
-    ref = ifft(fft(ref) * lp_filter)
-    obj = ifft(fft(obj) * lp_filter)
+    obj = band_pass_filter(time, obj)
+    ref = band_pass_filter(time, ref)
 
-    # apodising filter (time domain)
+    delta_phi = np.angle(obj * ref.conjugate()) / 2
+    delta_phi = delta_phi[len(delta_phi)*0.2:len(delta_phi)*0.8]
+    delta_phi = np.rad2deg(delta_phi)
+    return delta_phi
+
+def low_pass_filter(time, signal):
+    """Apply a low pass filter to the signal (np.array).
+
+    Returns np.array containing the low-pass filtered signal.
+    """ 
+    f = fftfreq(len(time), time[1]-time[0])
+    lp_filter = np.ones(len(signal)) / (1 + ((f / 6)**2))
+    return ifft(fft(signal) * lp_filter)
+
+def apodise(time, signal):
+    """Apodise the signal. Expects and returns np.arrays.
+    """
     a0 = 0.355768
     a1 = 0.487396
-    a2 = 0.144232  
+    a2 = 0.144232
     a3 = 0.012604
-    n = np.arange(0,len(time),1)
+    n = np.arange(0,len(time))
     apodize_filter =  (a0 - a1*np.cos(2*np.pi*n/len(time))
                       + a2*np.cos(4*np.pi*n/len(time))
                       - a3*np.cos(6*np.pi*n/len(time)))
-    ref *= apodize_filter
-    obj *= apodize_filter
+    return signal * apodize_filter
 
-    # bp filter in freq domain
-    sig = 1
-    f0 = 7
-    bp_filter = np.exp(-(f-f0)**2 / (2*sig*sig))
-    ref = ifft(fft(ref) * bp_filter)
-    obj = ifft(fft(obj) * bp_filter)
+def band_pass_filter(time, signal, freq=3.4, sigma=0.5):
+    """Apply band pass filter to signal.
 
-    delta_phi = np.angle(obj * ref.conjugate()) / 2
-    # exclude outer 25% because of edge effects
-    return np.mean(delta_phi[len(delta_phi)*0.25:len(delta_phi)*0.75])
+    Optionally specify the center frequency (freq, default 3.4)
+    and width (sigma, default 0.5).
+
+    Expects and returns np.arrays.
+    """
+    f = fftfreq(len(time), time[1]-time[0])
+    bp_filter = np.exp(-(f-freq)**2 / (2*(sigma**2)))
+    return ifft(fft(signal) * bp_filter)

polarimeter/test_polarimeter.py
@@ -1,7 +1,6 @@
 from __future__ import division
 import unittest
 import polarimeter
-import os
 import numpy as np
 
 class Test(unittest.TestCase):
@@ -9,76 +8,63 @@ class Test(unittest.TestCase):
 
 	def test_simulate_signals(self, repeat=1):
 		"""Test that simulate_signals, which is later used to test the
-		phase difference algorithm, behaves exactly as
-		polarimeter.acquire() does. Single measurement (no repeat).
+		phase difference algorithm, behaves as desired.
 		"""
-		n = 100
-		t, a, b = simulate_signals(np.arange(n), repeat=repeat)
-		self.assertIs(type(t), list)
-		self.assertIs(type(a), list)
-		self.assertIs(type(b), list)
+		time = np.arange(0, 5, 0.01)
+		sigA, sigB = simulate_signals(time)
+		self.assertIs(type(sigA), np.ndarray)
+		self.assertIs(type(sigB), np.ndarray)
+		self.assertEqual(len(sigA), len(time))
+		self.assertEqual(len(sigB), len(time))
 
-		self.assertEqual(len(t), n)
-		self.assertEqual(len(a), repeat)
-		self.assertEqual(len(b), repeat)
-
-		self.assertEqual(len(a[0]), n)
-		self.assertEqual(len(b[0]), n)
-
-	def test_simulate_multiple_signals(self):
-		"""Test that simulate_signals returns multiple signals for
-		"repeat" measurements.
-		"""
-		self.test_simulate_signals(repeat=3)
-
-	def test_simulated_analysis_no_noise(self):
+	"""Tests for polarimeter.calc_phase_difference"""
+	def test_CPD_simulated_analysis_no_noise(self):
 		"""Test analysis with a perfect, noiseless signal."""
 		phiB = np.pi * 0.33
-		t, sigA, sigB = simulate_signals(np.arange(0, 1, 0.01), phiB=phiB)
-		phi = polarimeter.calc_phase_difference(t, sigA, sigB)
-		self.assertEqual(round(phiB, 3), round(phi, 3))
+		time = np.arange(0, 3, 0.001)
+		sigA, sigB = simulate_signals(time, phiB=phiB)
+		phi = polarimeter.calc_phase_difference(time, sigA, sigB)
 
-	def test_simulated_analysis_with_noise(self):
-		"""Test analysis with a noisy simulated signal. Note that this
-		is different to no noise test in that it uses more data (longer
-		time period) and rounds to just 1 d.p. rather than 3.
-		"""
+		self.assertIs(type(phi), np.ndarray)
+		self.assertEqual(round(np.mean(np.deg2rad(phi)), 3),
+						 round(phiB, 3))
+
+	def test_CPD_simulated_analysis_with_noise(self):
+		"""Test analysis with a noisy signal."""
 		phiB = np.pi * 0.33
-		t, sigA, sigB = simulate_signals(np.arange(0, 10, 0.01), phiB=phiB,
-										 noise=0.02)
-		phi = polarimeter.calc_phase_difference(t, sigA, sigB)
-		self.assertEqual(round(phiB, 1), round(phi, 1))
+		time = np.arange(0, 3, 0.001)
+		sigA, sigB = simulate_signals(time, phiB=phiB, noise=0.01)
+		phi = polarimeter.calc_phase_difference(time, sigA, sigB)
 
-	def test_simulated_multiple_signal_analysis(self):
-		"""Test analysis with multiple perfect signals to check
-		analyse_data handles input correctly.
-		"""
-		phiB = np.pi * 0.33
-		t, chA, chB = simulate_signals(np.arange(0, 1, 0.01), repeat=5,
-									   phiB=phiB)
-		phi = polarimeter.calc_phase_difference(t, chA, chB)
+		self.assertIs(type(phi), np.ndarray)
+		self.assertTrue(len(phi) > 1)
+		self.assertEqual(round(np.mean(np.deg2rad(phi)), 3),
+						 round(phiB, 3))
 
-		for i in phi:
-			self.assertEqual(round(phiB, 3), round(i, 3))
+def simulate_signals(t, phiA=0, phiB=0.5, dcA=0.1, dcB=0.2,
+					  ampA=1, ampB=1.3, noise=0):
+	"""Simulate signals.
 
-def simulate_signals(t, f=3, phiA=0, phiB=0, dcA=-0.1, dcB=0.2,
-					  ampA=1, ampB=0.7, noise=0, repeat=1):
-	"""Simulate signals. Returns exactly as acquire does.
+	Angular rotational frequency f is set to 1.7 Hz as this is roughly
+	the frequency in practice. polarimeter.calc_phase_difference expects
+	this frequency as the band pass filter is centered there.
+
+	Arguments:
+		t: times (1D array)
+		phiA and phiB = initial phases (rads)
+		dcA and dcB = dc offsets
+		ampA and ampB = amplitudes
+		noise = noise factor
+
+	Returns:
+		sigA, sigB (np arrays)
 	"""
-	t = np.array(t)
-
-	sigA = []
-	sigB = []
-
-	while len(sigA) < repeat:
-		random_noise = np.random.normal(size=len(t))
-		sigA.append(list(ampA * (np.cos(2 * np.pi * f * t - phiA)) ** 2 + dcA
-				+ random_noise * noise))
-
-		random_noise = np.random.normal(size=len(t))
-		sigB.append(list(ampB * (np.cos(2 * np.pi * f * t - phiB)) ** 2 + dcB
-				+ random_noise * noise))
-	return list(t), sigA, sigB
+	f = 1.7
+	sigA = (ampA * (np.cos(2 * np.pi * f * t - phiA)) ** 2
+		+ dcA + np.random.normal(size=len(t)) * noise)
+	sigB = (ampA * (np.cos(2 * np.pi * f * t - phiB)) ** 2
+		+ dcB + np.random.normal(size=len(t)) * noise)
+	return sigA, sigB
 
 if __name__ == '__main__':
 	unittest.main()