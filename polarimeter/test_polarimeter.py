8bcb4bbaf24bbe4a06bbfcc760315cbf571b3c48
malgorzata <maglorzatanguyen@Malgorzatas-MacBook-Air.local>
2018-02-18 14:46:22 +0000

    cleanup simulators

--- polarimeter/test_polarimeter.py
+++ polarimeter/test_polarimeter.py
@@ -1,14 +1,14 @@
 from __future__ import division
 import unittest
 from polarimeter_fortest import Polarimeter
-from simulator_j import simulate_signals
+from simulator import simulate_signals, simulate_signals_j
 
 
 class Test(unittest.TestCase):
     """Tests for polarimeter.py"""
 
     def test_measure(self):
-        """Test p.measure()"""
+        """Test p.measure() with simulate_signals"""
         phase_difference = 60
         p = Polarimeter(source=simulate_signals,
                         sourceargs={'phase_difference': phase_difference})
@@ -17,5 +17,17 @@ class Test(unittest.TestCase):
         self.assertIsInstance(p.last_measured, int)
         self.assertIsInstance(p.phase_difference, float)
         self.assertAlmostEqual(p.phase_difference, phase_difference, places=5)
+
+    def test_measure_j(self):
+        """Test p.measure() with simulate_signals_j"""
+        phase_difference = 60
+        p = Polarimeter(source=simulate_signals_j,
+                        sourceargs={'phase_difference': phase_difference})
+        p.measure()
+        print p.phase_difference
+        self.assertIsInstance(p.last_measured, int)
+        self.assertIsInstance(p.phase_difference, float)
+        self.assertAlmostEqual(p.phase_difference, phase_difference, places=5)
+
 if __name__ == '__main__':
     unittest.main()
