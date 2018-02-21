b0f4aeb182f3a6d7b357c8e55d05b6af6da090c1
malgorzata <maglorzatanguyen@dyn1227-5.wlan.ic.ac.uk>
2018-02-21 16:28:37 +0000

    work on rerunning

--- polarimeter/rerunning.py
+++ polarimeter/rerunning.py
@@ -1,8 +1,14 @@
 import os
 import numpy as np
+from polarimeter import Polarimeter
 
-for filename in os.listdir("/Users/maglorzatanguyen/Desktop/results_trace1"):
-    if filename.endswith(".txt"):
-        np.loadtxt(filename, delimiter=',', dtype=np.dtype('d'), unpack=True)
-        
-
+results_file = "/Users/maglorzatanguyen/Desktop/rerun.txt"
+path = "/Users/maglorzatanguyen/Desktop/results_trace1"
+for filename in os.listdir(path):
+    with open(results_file, 'w') as f:
+        if filename.endswith(".txt"):
+            filepath = os.path.join(path + "/" + filename)
+            data = np.loadtxt(filepath, delimiter=',', dtype=np.dtype('d'), unpack=True)
+            p = Polarimeter(source=data, sourceargs={'capture_time': 1, })
+            p.measure()
+            f.write("{},{},{}\n".format(p.last_measured, p.phase_difference, p.stdeviation))
