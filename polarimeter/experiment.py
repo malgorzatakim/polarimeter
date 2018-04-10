3e427585a1d25738317baafe9faa613d763a08e6
malgorzata <maglorzatanguyen@Malgorzatas-MacBook-Air.local>
2018-04-10 23:32:13 +0100

    class Runner added

--- polarimeter/experiment.py
+++ polarimeter/experiment.py
@@ -1,88 +1,116 @@
-from data_acquirers import RealDataAcquirer
+from argparse import ArgumentParser
+
+from data_acquirers import RealDataAcquirer, SimulatedDataAcquirer, RecordedDataAcquirer
 from polarimeter import Polarimeter
+import time
+import os
+import numpy as np
 
-capture_time = 1
-acquirer = RealDataAcquirer(capture_time)
-p = Polarimeter()
-phase_diff, stdev = p.measure(*acquirer.acquire())
+class Runner():
+    def experiment(self):
+        capture_time = 1
+        acquirer = RealDataAcquirer(capture_time)
+        p = Polarimeter()
+        phase_diff, stdev = p.measure(*acquirer.acquire())
 
-print('{} +/- {} at {}'.format(phase_diff, stdev, acquirer.lastDataTimestamp()))
+        print('{} +/- {} at {}'.format(phase_diff, stdev, acquirer.lastDataTimestamp()))
 
+    def exper_time(self):
+        capture_time = 1
+        acquirer = RealDataAcquirer(capture_time)
+        p = Polarimeter()
+        x = 5 # how many flushes to file
+        y = 10 # experiment count per one flush
+        timestamp = int(time.time())
+        #results_file = os.path.join("/Users/maglorzatanguyen/Desktop/results/" + str(timestamp) + ".txt")
+        results_file = os.path.join("C:\\polarimeter\\results\\" + str(timestamp) + ".txt")
 
-# import os
+        with open(results_file, "w") as f:
+            for _ in range(x):
+                for _ in range(y):
+                    data = acquirer.acquire()
+                    phase_diff, stdev = p.measure(*data)
+                    f.write("{},{},{}\n".format(acquirer.lastDataTimestamp(), phase_diff, stdev))
+                #time.sleep(300)
+                f.flush()
 
+    def exper_time2(self):
+        phase_difference = 60
+        acquirer = SimulatedDataAcquirer(phase_difference=phase_difference)
+        p = Polarimeter()
 
-# def pretty_print_result(timestamp, phase_difference):
-#     line = '{:s}, {:07.3f} degrees'
-#     print(line.format(time.asctime(time.gmtime(timestamp)),
-#                       phase_difference))
+        x = 6 # how many flushes to file, no of data points
+        y = 10 # experiments taken into mean
+        timestamp = int(time.time())
+        results_file = os.path.join("/Users/maglorzatanguyen/Desktop/results/" + str(timestamp) + ".txt")
+        #results_file = os.path.join("C:\\polarimeter\\results\\" + str(timestamp) + ".txt")
+        #### build string from 3 parts: path to file (basic name), timestamp, ".txt"
 
+        with open(results_file, "w") as f:
+            for _ in range(x):
+                t = int(time.time())
+                data = []
+                for _ in range(y):
+                    phase_diff, stdev = p.measure(*acquirer.acquire())
+                    data.append([acquirer.lastDataTimestamp(), phase_diff, stdev])
+                phase = []
+                stdev = []
+                for i in range(len(data)):
+                    phase.append(data[i][1])
+                    stdev.append(data[i][2])
+                f.write("{},{},{}\n".format(t, np.mean(phase), np.mean(stdev)))
+                f.flush()
 
-# def run(output_file=None, duration=30, units='s', print_output=True,
-#         log_signals=False):
-#     """Repeatedly run measurements for specified duration.
+    def rerunning(self):
+        results_file = "C:\\polarimeter\\results_trace_rerunning\\results\\result_rerunning.txt"
+        #results_file = "/Users/malgorzatanguyen/Desktop/rerun.txt"
+        with open(results_file, 'w') as f:
+            data_folder_path = "C:\\polarimeter\\results_trace_rerunning\\"
+            #data_folder_path = "/Users/malgorzatanguyen/Desktop/results_trace1/"
+            acquirer = RecordedDataAcquirer(data_folder_path)
+            p = Polarimeter()
+            while acquirer.isDataAvailable():
+                data = acquirer.acquire()
+                phase_diff, stdev = p.measure(*data)
+                f.write("{},{},{}\n".format(acquirer.lastDataTimestamp(), phase_diff, stdev))
 
-#     Returns mean and std of phase differences, and number of measurements.
+    def laser_stability(self):
+        x = 7000 #how many flushes to file
+        y = 10 #no of experiments in a single flush
+        timestamp = int(time.time())
+        results_file = os.path.join(
+            "C:\\polarimeter\\results\\" + str(timestamp) + "_stab.txt")
 
-#     Parameters
-#     ----------
-#     output_file : str, optional
-#         Filename where to save timestamp and phase difference.
-#     duration : int
-#         Time to repeat measurements for
-#     units: str
-#         's'(econds), 'm'(inutes), 'h'(ours) or 'd'(ays).
-#     print_output: bool
-#         If true, print time and phase difference; default: True.
-#     log_signals: bool
-#         Save raw signal data; default: False.
+        with open(results_file, "w") as f:
+            for _ in range(x):
+                for _ in range(y):
+                    times = int(time.time())
+                    acquirer = RealDataAcquirer(1)
+                    one_acq = acquirer.acquire()
+                    chA = one_acq[:,1]
+                    chB = one_acq[:,2]
+                    final_chA = np.mean(chA)
+                    final_chB = np.mean(chB)
+                    f.write("{},{},{}\n".format(times, final_chA, final_chB))
+            f.flush()
 
-#     Example
-#     -------
-#     run('foo.txt', duration=3, units='s')
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("scenario", nargs=1)
+    args = parser.parse_args()
+    scenario = args.scenario[0]
+    if scenario == 'experiment':
+        Runner().experiment()
+    elif scenario == 'exper_time':
+        Runner().exper_time()
+    elif scenario == 'exper_time2':
+        Runner().exper_time2()
+    elif scenario == 'laser_stability':
+        Runner().laser_stability()
+    elif scenario == 'rerunning':
+        Runner().rerunning()
+    else:
+        raise Exception("Uknown scenario: {}".format(args.scenario))
 
-#     """
-
-#     if output_file is None:
-#         output_file = 'temp.txt'
-
-#     # Convert time into seconds
-#     if units == 's': # seconds
-#         pass
-#     elif units == 'm': # minutes
-#         duration *= 60
-#     elif units == 'h': # hours
-#         duration *= 60 * 60
-#     elif units == 'd': # days
-#         duration *= 24 * 60 * 60
-#     else:
-#         raise ValueError(('Unit must be seconds (s), minutes (m), hours (h)'
-#                           ' or days (d).'))
-
-#     start = time.time()
-
-#     # repeatedly acquire data
-#     while time.time() < start + duration:
-#         t, phi = measure(save_data=log_signals)
-#         write_result(output_file, t, phi)
-
-#         if print_output:
-#             pretty_print_result(t, phi)
-
-#     # load all the data to compute stats
-#     t, phi  = np.loadtxt(output_file, delimiter=',', unpack=True)
-
-#     if output_file is 'temp.txt':
-#         os.remove('temp.txt')
-
-#     return np.mean(phi), np.std(phi, ddof=1), len(phi)
-
-# def write_result(filename, timestamp, phase_difference):
-#     """Append to filename the timestamp and phase difference.
-#     Returns line that was written as a string.
-#     """
-#     f = open(filename, 'a')
-#     string = '{}, {:.15f}\n'.format(timestamp, phase_difference)
-#     f.write(string)
-#     f.close()
-#     return string
+if __name__ == "__main__":
+    main()
